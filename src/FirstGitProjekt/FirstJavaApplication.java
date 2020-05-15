334002436a697f1c2d5bee1b720405d2f0539cd3
PWRoman <45557753+PWRoman@users.noreply.github.com>
2020-05-15 09:17:25 +0200

    First Comment

--- src/FirstGitProjekt/FirstJavaApplication.java
+++ src/FirstGitProjekt/FirstJavaApplication.java
@@ -14,6 +14,7 @@ public class FirstJavaApplication {
     public void main(String[] args) {
         
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
     
 }
